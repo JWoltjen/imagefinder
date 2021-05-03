f4644adf10a7edd7baccb06808d652e70816d592
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-03 10:37:21 -0500

    create imagecard.js component and map through all images in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'; 
-
+import ImageCard from './components/ImageCard'
 
 
 function App() {
@@ -20,37 +20,11 @@ function App() {
   }, [])
 
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src="https://source.unsplash.com/random" alt="a random image" className='w-full'/>
-      <div className="px-6 py=4">
-        <div className='font-bold text-purple-500 text-xl mb-2'>
-          photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            9000
-          </li>
-            <li>
-            <strong>Downloads: </strong>
-            9000
-          </li>
-            <li>
-            <strong>Likes: </strong>
-            9000
-          </li>
-        </ul>
-      </div>
-      <div className='px-6 py-4'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag1
-        </span>
-           <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag2
-        </span>
-           <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className='grid grid-cols-3 gap-4'>
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+  ))}
       </div>
     </div>
   );
